dde5e0b3eae070617c99e7567f4d34d2d49e637c
y-sone <snowberry815@gmail.com>
2023-03-18 20:03:48 +0900

    接続用の設定を追加

---
+++ app/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '/usr/src/myapp/vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'db',
+    'database' => 'eloquent_test',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
