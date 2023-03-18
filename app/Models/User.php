7515291e87ae237765ff90eeca33e9f1a019409c
y-sone <snowberry815@gmail.com>
2023-03-18 20:04:17 +0900

    テスト用のModelと実行ファイルを追加

---
+++ app/Models/User.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '/usr/src/myapp/app/connection.php';
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+}
---
+++ app/main.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'connection.php';
+require_once 'Models/User.php';
+
+/**
+ * 準備
+ * dbコンテナで下記を実行
+ * - create database eloquent_test;
+ * - create table eloquent_test.users (id int, email varchar(50));
+ * - insert into eloquent_test.users values(1, 'test@example.com');
+ */
+$users = $users = User::find(1);
+var_dump('id: ' . $users->id);
+var_dump('email: ' . $users->email);
